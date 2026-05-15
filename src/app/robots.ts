367d61c92eb6ab359661c7443fe582250f68929b
Yasas Sadeepa <yasassadeepa868@gmail.com>
2026-05-15 18:31:33 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,17 @@ import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
-    },
+    rules: [
+      {
+        userAgent: 'facebookexternalhit',
+        allow: '/',
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: '/private/',
+      },
+    ],
     sitemap: 'https://closerintellect.ai/sitemap.xml',
   };
 }
