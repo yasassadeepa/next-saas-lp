f6d152f201f7d8b76d7b7ba8bbd123af3c47c00e
Yasas Sadeepa <yasassadeepa868@gmail.com>
2026-05-17 12:55:46 +0530

    smooth scrolling

--- src/providers/lenis.tsx
+++ src/providers/lenis.tsx
@@ -3,11 +3,12 @@
 import BlurEffect from '@/components/blur-effect'
 import { ReactLenis } from 'lenis/react'
 import { LazyMotion, domAnimation } from "motion/react"
+import 'lenis/dist/lenis.css'
 
 export default function LenisProvider({ children }: { children: React.ReactNode }) {
   return (
     <LazyMotion features={domAnimation}>
-      <ReactLenis root options={{ duration: 1.3 }}>
+      <ReactLenis root options={{ duration: 1.3, autoRaf: true }}>
         <BlurEffect />
         {children}
       </ReactLenis>
